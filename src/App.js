34aa38d80a14eabf23bb4896fa87fffcfa9b3377
ranjitkshah <ranjitmaheshshah@gmail.com>
2020-05-24 02:08:02 +0530

    single page

--- src/App.js
+++ src/App.js
@@ -7,23 +7,54 @@ import QuizPage from './Components/QuizPage';
 import VideoLecturePage from './Components/VideoLecturePage';
 import LiveSchedule from './Components/LiveSchedule';
 import AssignmentPage from './Components/AssignmentPage';
+import SpaceBar from './Components/Utilities/SpaceBar';
 import LiveStream from './Components/LiveStream';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import APIFetcher from './Components/Utilities/APIFetcher'
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/Demo">
 
-        <Route path='/' component={Navbar} />
+        {/* <Route path='/' component={Navbar} />
         <Route path='/' component={DashBar} />
         <Route path='/' exact component={LoginPage} />
         <Route path='/schedule' exact component={LiveSchedule} />
         <Route path='/quiz' exact component={QuizPage} />
         <Route path='/assignment' exact component={AssignmentPage} />
         <Route path='/video' exact component={VideoLecturePage} />
-        <Route path='/lesson' exact component={LessonPage} />
-        {/* <LiveStream /> */}
+        <Route path='/lesson' exact component={LessonPage} /> */}
+         <Navbar />
+        <DashBar />  
+        <LoginPage /> 
+        <SpaceBar /> 
+         <Navbar />
+        <DashBar />  
+        <LessonPage />
+        <SpaceBar /> 
+         <Navbar />
+        <DashBar />  
+        <VideoLecturePage />
+        <SpaceBar /> 
+         <Navbar />
+        <DashBar />  
+        <QuizPage />
+        <SpaceBar /> 
+         <Navbar />
+        <DashBar />  
+        <AssignmentPage />
+        <SpaceBar /> 
+         <Navbar />
+        <DashBar />  
+        <LiveSchedule />
+        <SpaceBar /> 
+         <Navbar />
+        <DashBar />  
+        <LiveStream />
+        <SpaceBar /> 
+         
+
+
         <APIFetcher></APIFetcher>
       </Router>
     </div>
---
+++ src/Components/Utilities/SpaceBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function SpaceBar() {
+    return (
+        <div style={{height: "30px", backgroundColor:' black', margin:"10px 0", padding:"0"}}>
+            
+        </div>
+    )
+}
+
+export default SpaceBar
